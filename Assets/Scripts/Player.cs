6ce0b764d3b2635606e6784b831d22aac3564290
Shuzhan Xie <swqa7697@gmail.com>
2018-04-25 15:36:29 -0700

    Testing

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player: MonoBehaviour {
+	public float mySpeed;
+
+	void Awake () {
+		
+	}
+
+	void Update () {
+		
+	}
+}
