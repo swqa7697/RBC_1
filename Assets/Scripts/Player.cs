391ffea17551042756fd06b4986d4ad0972f1a36
Shuzhan Xie <swqa7697@gmail.com>
2018-04-26 13:20:05 -0700

    Player (Move, Jump)
    Jumping is still looks strange. Grab will be implemented soon.

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -3,14 +3,14 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Player: MonoBehaviour {
-	public float mySpeed, jumpSpeed;
+    public float mySpeed, jumpSpeed;
 
-    private Rigidbody myRigidbody;
+    private Rigidbody2D myRigidbody;
     private bool grounded = true;
 
 	private void Awake()
     {
-        myRigidbody = GetComponent<Rigidbody>();
+        myRigidbody = GetComponent<Rigidbody2D>();
 	}
 
     private void FixedUpdate()
@@ -20,39 +20,44 @@ public class Player: MonoBehaviour {
 
     private void Update()
     {
-        transform.rotation = Quaternion.LookRotation(Vector3.forward, Vector3.up);
+//        transform.rotation = Quaternion.LookRotation(Vector3.forward, Vector3.up);
         if (Input.GetKeyDown(KeyCode.Space))
             Jump();
     }
 
-	private void OnCollisionEnter(Collision other)
-	{
-//		if (other.gameObject.tag != "Player")  //The tag may be changed
-//		{
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+		if (collision.gameObject.tag == "Standable")  //All things can be stood on will have this tag
+		{
 			grounded = true;
-//		}
+		}
 	}
 
     private void Move()
     {
-        Vector3 input = Vector3.zero;
-		if (Input.GetKey (KeyCode.LeftArrow))
-			input += Vector3.left;
-		if (Input.GetKey (KeyCode.RightArrow))
-			input += Vector3.right;
+        Vector2 input = Vector2.zero;
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            input += Vector2.left;
+            myRigidbody.transform.eulerAngles = new Vector3(0f, 180f);
+        }
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            input += Vector2.right;
+            myRigidbody.transform.eulerAngles = new Vector3(0f, 0f);
+        }
 		
         input.Normalize();
 
-        Vector3 newPosition = myRigidbody.position + input * mySpeed * Time.deltaTime;
+        Vector2 newPosition = myRigidbody.position + input * mySpeed * Time.deltaTime;
         myRigidbody.MovePosition(newPosition);
     }
 
     private void Jump()
     {
-        Debug.Log(grounded);
         if (grounded == true)
         {
-            myRigidbody.AddForce(Vector3.up * jumpSpeed);
+            myRigidbody.AddForce(Vector2.up * jumpSpeed);
             grounded = false;
         }
     }
