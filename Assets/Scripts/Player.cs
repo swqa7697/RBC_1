c4d60f67c3f2ef7685edeaec4b5f56929aa1ccea
Shuzhan Xie <swqa7697@gmail.com>
2018-04-25 17:01:15 -0700

    Initialize

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -3,13 +3,49 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Player: MonoBehaviour {
-	public float mySpeed;
+	public float mySpeed, jumpSpeed;
 
-	void Awake () {
-		
+    private Rigidbody myRigidbody;
+    private bool grounded = true;
+
+	private void Awake()
+    {
+        myRigidbody = GetComponent<Rigidbody>();
 	}
 
-	void Update () {
-		
-	}
+    private void FixedUpdate()
+    {
+        Move();
+    }
+
+    private void Update()
+    {
+//        transform.rotation = Quaternion.LookRotation(Vector3.forward, Vector3.up);
+        if (Input.GetKeyDown(KeyCode.Space))
+            Jump();
+    }
+
+    private void Move()
+    {
+        Vector3 input = Vector3.zero;
+        if (Input.GetKey(KeyCode.LeftArrow))
+            input += Vector3.left;
+        if (Input.GetKey(KeyCode.RightArrow))
+            input += Vector3.right;
+
+        input.Normalize();
+
+        Vector3 newPosition = myRigidbody.position + input * mySpeed * Time.deltaTime;
+        myRigidbody.MovePosition(newPosition);
+    }
+
+    private void Jump()
+    {
+        Debug.Log(grounded);
+        if (grounded == true)
+        {
+            myRigidbody.AddForce(Vector3.up * jumpSpeed);
+            grounded = false;
+        }
+    }
 }
