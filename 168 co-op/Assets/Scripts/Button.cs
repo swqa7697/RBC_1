5905602d2823cf8f828b2c6310216d87e57def8b
Richie Poon <richiep@uci.edu>
2018-04-29 00:12:33 -0700

    changed 168 co op
    lotsa stuff

---
+++ 168 co-op/Assets/Scripts/Button.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Button : MonoBehaviour {
+
+	private GameObject Door;
+
+	public bool open = false;
+
+	void Start () {
+		Door = GameObject.FindGameObjectWithTag ("door");
+	}
+
+	void FixedUpdate (){
+		if (open == true) {
+			Door.gameObject.SetActive (false);
+		} else {
+			Door.gameObject.SetActive (true);
+		}
+	}
+
+	void OnTriggerEnter2D(Collider2D col){
+		open = true;
+	}
+
+	void OnTriggerStay2D(Collider2D col){
+		open = true;
+	}
+
+	void OnTriggerExit2D(Collider2D col){
+		open = false;
+	}
+}
