bf533ab92d4221db0e264a9f500255451fd45cfb
Brett Galkowski <bgalkows@uci.edu>
2018-04-30 18:04:33 -0700

    Basic Networking
    oh god this swarm of meta file changes will be the death of me

--- 168 co-op/Assets/Scripts/PlayerController.cs
+++ 168 co-op/Assets/Scripts/PlayerController.cs
@@ -2,14 +2,19 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.Networking;
 
-public class PlayerController : MonoBehaviour {
+public class PlayerController : NetworkBehaviour {
 
 	public float jumpPower = 150f;
 	public float maxSpeed = 10f;
 
 	private SpriteRenderer PSR;
 	public Sprite[] PS;
+
+    public Sprite localRed1;
+    public Sprite localRed2;
+
 
 	//public GameObject GameOverUI;
 
@@ -24,22 +29,44 @@ public class PlayerController : MonoBehaviour {
 		//GameOverUI.SetActive(false);
 		rb2d = gameObject.GetComponent<Rigidbody2D> ();
 		PSR = gameObject.GetComponent<SpriteRenderer>();
+        
 	}
 
+    public override void OnStartLocalPlayer()
+    {
+        rb2d = gameObject.GetComponent<Rigidbody2D>();
+        PSR = gameObject.GetComponent<SpriteRenderer>();
+
+        Debug.Log("IS CALLED");
+        Debug.Log(localRed1);
+        PSR.sprite = localRed1;
+
+    }
+
 	void FixedUpdate () {
+
+        if (!isLocalPlayer)
+            return;
+
 		float h = Input.GetAxis ("Horizontal");
 		rb2d.velocity = new Vector2 (h * maxSpeed, rb2d.velocity.y);
 
-		if (grabbing == true) {
-			PSR.sprite = PS [1];
-		} else {
-			PSR.sprite = PS [0];
-		}
+        if (grabbing == true)
+        {
+            PSR.color = Color.black;
+        }
+		//	PSR.sprite = PS [1];
+		//} else {
+		//	PSR.sprite = PS [0];
+		//}
 
 
 	}
 
 	void Update () {
+
+       if (!isLocalPlayer)
+            return;
 
 		if (Input.GetButtonDown ("Jump")) {
 			if (grounded == true) {
