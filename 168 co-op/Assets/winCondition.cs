d3b52dd7b212a022127b6868a24e4467d41c7b79
Brett Galkowski <bgalkows@uci.edu>
2018-04-30 20:21:51 -0700

    networked level management

---
+++ 168 co-op/Assets/winCondition.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class winCondition : MonoBehaviour {
+
+
+    [SerializeField]
+    private int playerCount;
+    public string nextLevelName;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		if(playerCount >= 2)
+        {
+            levelManager lev = GameObject.FindGameObjectWithTag("GameController").GetComponent<levelManager>();
+            lev.loadScene(nextLevelName);
+
+        }
+	}
+
+    private void OnTriggerEnter2D(Collider2D col)
+    {
+        if(col.gameObject.tag == "Player")
+        {
+            playerCount++;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D col)
+    {
+        if(col.gameObject.tag == "Player")
+        {
+            playerCount--;
+        }
+    }
+}
